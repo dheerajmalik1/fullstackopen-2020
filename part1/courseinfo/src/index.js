0147595a7adf6f4c0ceca50a02c0d10d87b3bac1
dheerajmalik1 <dheerajmalikwork@gmail.com>
2020-03-17 15:03:32 +0530

    course information step1

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,12 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+const Header = (props) => {return(<div>{props.course}</div>)}
+const Content = (props) => {return <div>{props.name} {props.exercises}</div>}
+const Total= (props) => {return <div>{props.sum}</div>}
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
 
-ReactDOM.render(<App />, document.getElementById('root'));
+  return (
+    <div>
+      <h1>
+        <Header course ={course}/>
+      </h1>
+      <p>
+        <Content name={part1} exercises={exercises1}/>
+      </p>
+      <p>
+        <Content name={part2} exercises={exercises2}/>
+      </p>
+      <p>
+        <Content name={part3} exercises={exercises3}/>
+      </p>
+      <p>Number of exercises<Total sum ={exercises1 + exercises2 + exercises3} /></p>
+    </div>
+  )
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App />, document.getElementById('root'))
