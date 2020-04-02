0ef44b751c8daf7f2f072c2e5686797646a608a6
dheerajmalik1 <dheerajmalikwork@gmail.com>
2020-04-02 15:51:14 +0530

    1.7: unicafe step2

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -17,7 +17,8 @@ const App = () => {
   const [bad, setBad] = useState(0)
   
   const all = good+neutral+bad;
-  console.log(all);
+  const avg =((good*1)+(neutral*0)+ (bad*-1))/all;
+  const positive =good/all;
   return (
     
     <div>
@@ -30,6 +31,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>  
+      <p>average {avg}</p>  
+      <p>positive {positive}</p>  
     </div>
   )
 }
